717370a2062faf70c178bc78e991bc221121d237
Lord9s <nkunajunior399@gmail.com>
2024-12-02 13:06:45 +0200

    Update help.js

--- scripts/cmds/help.js
+++ scripts/cmds/help.js
@@ -3,7 +3,7 @@ const axios = require("axios");
 const path = require("path");
 const { getPrefix } = global.utils;
 const { commands, aliases } = global.GoatBot;
-const doNotDelete = "[ QueenBotV2 ]";
+const doNotDelete = "[ KingBotV2 ]";
 
 module.exports = {
     config: {
